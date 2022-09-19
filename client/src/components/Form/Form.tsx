14e43f8da81ab7de9a44753e9f3385439537809b
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-19 22:39:33 +0400

    Forms visualization and most of the functionality done

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -16,14 +16,24 @@ const Form = () => {
     });
   };
 
+  const clearData = () => {
+    setFormData({
+      creator: "",
+      title: "",
+      message: "",
+      selectedFile: "",
+      tags: "",
+    });
+  };
+
   console.log(formData);
 
   const handleSubmit = () => {};
 
   return (
-    <div className=" bg-gray-50 w-[30%] rounded-[8px] pb-10 pt-5">
+    <div className=" bg-gray-50 w-[30%] rounded-[8px] pb-10 pt-5 flex flex-col items-center">
       <form
-        className="flex flex-col items-center"
+        className="flex flex-col items-center w-full"
         onSubmit={() => handleSubmit()}
       >
         <h1 className="font-bold sm:text-xl text-sm text-center">
@@ -46,11 +56,34 @@ const Form = () => {
         <input
           type="text"
           placeholder="Message"
-          className="shadow appearance-none border mt-5 rounded w-[80%] py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border mt-5 rounded w-[80%] py-2 px-3 pb-10 text-black leading-tight focus:outline-none focus:shadow-outline"
           value={formData.message}
           onChange={(e) => changeFormData(e.target.value, "message")}
         />
+        <input
+          type="text"
+          placeholder="Tags (Separate By Comas)"
+          className="shadow appearance-none border mt-5 rounded w-[80%] py-2 px-3  text-black leading-tight focus:outline-none focus:shadow-outline"
+          value={formData.tags}
+          onChange={(e) => changeFormData(e.target.value, "tags")}
+        />
+        <input
+          type="file"
+          className="shadow appearance-none border file:border-none file:bg-blue-400 file:py-2 file:px-3 file:text-white file:rounded-[8px] mt-5 rounded w-[80%] py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+          // value={formData.tags}
+          // onChange={(e) => changeFormData(e.target.value, "tags")}
+        />
+
+        <button className="w-[90%] bg-blue-400 text-white py-2 px-3 rounded-[8px] mt-5 text-lg">
+          Submit
+        </button>
       </form>
+      <button
+        className="w-[90%] bg-red-400 text-white py-2 px-3 rounded-[8px] mt-5 text-lg"
+        onClick={() => clearData()}
+      >
+        Clear
+      </button>
     </div>
   );
 };
